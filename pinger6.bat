7a90ac41c714aca5bc903a67f52c792a4d07a0cf
natrix_dev <discordlenko@gmail.com>
2022-01-11 18:44:02 +0100

    Update pinger6.bat

--- pinger6.bat
+++ pinger6.bat
@@ -3,6 +3,10 @@ color a
 echo starting ...
 cls
 @echo off
-set /p WEBSITE_ADRESS=Website Adress (ex: antrix.com)?
-ping %WEBSITE_ADRESS
+set /p WEBSITE_ADRESS=Website Adress (ex: natrix.com)?
+ping %WEBSITE_ADRESS%
+
+@echo Pinger by natrix has done its work !
+@echo See you soon 
+tilte Work Done !
 pause
